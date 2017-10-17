2269eec54174eee21862472373ca19f779eba1d9
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-10-17 14:48:37 +0200

    Affiliation remove capitalization

--- js/members.js
+++ js/members.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
         var items = [];
         $.each(data, function(i) {
             member = data[i];
-            member["affiliation"] = titleCase(member["affiliation"]);
             member["fullname"] = titleCase(member["First name"] + " " +member["Last name"]);
             member["link"] = "https://www.google.com/search?q="+member["fullname"]+" "+member["affiliation"];
             items.unshift("<div class='col-sm-3 member'><a href='"+member["link"]+"' target='_blank' class='fullname'><b>" + member["fullname"]+"</b></a><br/><span class='affiliation'>"+member["affiliation"]+"</span><br/><span class='country'>"+member["country"]+"</span></div>");
