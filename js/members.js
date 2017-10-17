3d069072ed4e880e643233ee8602ffef1dcd6486
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-10-17 14:36:37 +0200

    Full name capitalization

--- js/members.js
+++ js/members.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var items = [];
         $.each(data, function(i) {
             member = data[i];
-            member["fullname"] = member["First name"] + " " +member["Last name"]
+            member["fullname"] = titleCase(member["First name"] + " " +member["Last name"])
             member["link"] = "https://www.google.com/search?q="+member["fullname"]+" "+member["affiliation"]
             items.unshift("<div class='col-sm-3 member'><a href='"+member["link"]+"' target='_blank' class='fullname'><b>" + member["fullname"]+"</b></a><br/><span class='affiliation'>"+member["affiliation"]+"</span><br/><span class='country'>"+member["country"]+"</span></div>");
         });
@@ -11,3 +11,14 @@ $(document).ready(function() {
 
     });
 });
+
+
+function titleCase(str) {
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        })
+        .join(' ');
+}
