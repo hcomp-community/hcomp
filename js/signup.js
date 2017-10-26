63a4287900afb6c90a4b5f5911bf96d2d782cebe
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-10-26 11:45:44 +0200

    email signup instead of form sign up

---
+++ js/signup.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#sign-up-button").click(function(){
+        var email = $("#sign-up-email").val();
+        $.post("https://hcomp-slack-invite.herokuapp.com/invite", {"email":email}, function(response){
+            $("#sign-up-message").html(response["message"]);
+        });
+    });
+});
